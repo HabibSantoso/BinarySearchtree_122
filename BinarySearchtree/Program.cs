c118cdce46bbb1897f1791e46b67fd2f46c14233
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2022-12-30 17:17:34 +0700

    menambahkan class node beserta contruktornya

--- BinarySearchtree/Program.cs
+++ BinarySearchtree/Program.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace BinarySearchtree
 {
+    class Node 
+    {
+        public String info;
+        public Node leftchild;
+        public Node rightchild;
+
+        // contructor
+        public Node(String i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
